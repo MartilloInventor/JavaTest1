ffb9097cdfbcfddd073129ad9d9069730ec9cbad
Joachim Martillo <Joachim.CS.Martillo@gmail.com>
2021-08-22 15:01:14 -0400

    Initial commit -- simple test environment

---
+++ src/main/java/net/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package net.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        double testvar = 1;
+        System.out.println("Hello, World! " + testvar);
+        System.out.println("Exiting.");
+    }
+}
